c40c505d55ef223192cc175e427c178f42603b2b
Christian Schefuss <christian.schefuss@swisscom.com>
2015-11-18 12:08:15 +0100

    Adds process type classes

---
+++ src/biz.dfch.CS.Activiti.Client/ProcessDefinitionsResponse.cs
@@ -0,0 +1,59 @@
+﻿/**
+ * Copyright 2015 d-fens GmbH
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+
+namespace biz.dfch.CS.Activiti.Client
+{
+
+    #region activiti process-definitions v5.16.4.0
+    public class ProcessDefinitionsResponseData
+    {
+        [Required]
+        public string id { get; set; }
+        [Required]
+        public string url { get; set; }
+        public int version { get; set; }
+        public string key { get; set; }
+        public string category { get; set; }
+        public bool suspended { get; set; }
+        [Required]
+        public string name { get; set; }
+        public string description { get; set; }
+        public string deploymentId { get; set; }
+        public string deploymentUrl { get; set; }
+        public bool graphicalNotationDefined { get; set; }
+        public string resource { get; set; }
+        public string diagramResource { get; set; }
+        public bool startFormDefined { get; set; }
+
+    }
+        
+    public class ProcessDefinitionsResponse
+    {
+        public List<ProcessDefinitionsResponseData> data { get; set; }
+        public int size { get; set; }
+        public string order { get; set; }
+        public string sort { get; set; }
+        public int start { get; set; }
+        public int total { get; set; }
+
+    }
+    #endregion
+
+}
---
+++ src/biz.dfch.CS.Activiti.Client/ProcessInstancesResponse.cs
@@ -0,0 +1,56 @@
+﻿/**
+ * Copyright 2015 d-fens GmbH
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+
+namespace biz.dfch.CS.Activiti.Client
+{
+
+    #region activiti process-instances v5.16.4.0
+    public class ProcessInstancesResponseData
+    {
+        [Required]
+        public string id { get; set; }
+        [Required]
+        public string url { get; set; }
+        public string businessKey { get; set; }        
+        public bool suspended { get; set; }
+        public bool ended { get; set; }
+        public bool completed { get; set; }
+        [Required]
+        public string processDefinitionId { get; set; }
+        public string processDefinitionUrl { get; set; }
+        public string activityId { get; set; }
+        public string tenantId { get; set; }
+        public List<ProcessVariables> variables { get; set; }
+
+    }
+
+    public class ProcessInstancesResponse
+    {
+        public List<ProcessInstancesResponseData> data { get; set; }
+        public int size { get; set; }
+        public string order { get; set; }
+        public string sort { get; set; }
+        public int start { get; set; }
+        public int total { get; set; }
+
+    }
+    #endregion
+
+}
